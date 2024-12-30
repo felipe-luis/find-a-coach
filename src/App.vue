8212e82cd35568422ec06507be156daa587de10e
felipe-luis <felipellopes12@hotmail.com>
2024-12-30 16:43:14 -0300

    Adiciona o header da aplicação e a funcionalidade de troca de páginas dele.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
+	<TheHeader></TheHeader>
 	<RouterView></RouterView>
 </template>
 
 <script>
+import TheHeader from './components/layout/TheHeader.vue';
 
-import { RouterView } from 'vue-router';
-
+export default{
+	components:{
+		TheHeader,
+	}
+}
 </script>
 
-<style></style>
+<style>
+
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
+}</style>
